e0e58f1604fc43793e60921e402c75d7a74d61f0
Paul Marro <p_marro@u.pacific.edu>
2024-10-06 22:48:23 -0700

    basic timer works

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,7 +1,9 @@
+import javax.swing.*;
 import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
@@ -15,7 +17,14 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		Timer someTimerVar = new Timer(1000, this);
+		someTimerVar.start();
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.move(5, 0);
+	}
+	
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
